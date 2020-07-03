c1b4edc87543d83d00c684a0800aafcf54300280
Michael <lovem@DESKTOP-16M9J9A.localdomain>
2020-07-03 13:55:35 -0500

    App work space

---
+++ showmechange/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import BackgroundImage from './BackgroundImage';
+import City from '../src/City.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-image">
+      <header className="App-Image">
+        <img src={City} className="city-image" alt="St.Louis" />
+        <p>
+          Show Me Change.
+        </p>
+        <a
+         
+          
+          
+         
+        >
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
