d58d0f7525e277c9bdb697a7c6ae08690c577fe5
Lovem19 <66986461+Lovem19@users.noreply.github.com>
2020-07-19 20:59:48 -0500

    copy changes from duplicate clone branch

--- showmechange2/src/App.js
+++ showmechange2/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import City from './City.png';
+//import { isCompositeComponentWithType } from 'react-dom/test-utils';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={City} className="App-logo" alt="logo" />
         <p>
-          Show me change
+          Show Me Change
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+         
+          
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
