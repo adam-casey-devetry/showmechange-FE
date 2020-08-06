c01d28533948829d823a8786d813c0da7681432b
Lovem19 <66986461+Lovem19@users.noreply.github.com>
2020-08-05 19:47:28 -0500

    posted list of caps

--- showmechange2/src/ListOfCapitals.js
+++ showmechange2/src/ListOfCapitals.js
@@ -13,4 +13,47 @@ const listOfCapitals = {
     "Salt Lake City": "black"
 }
 
-export default 
+export default function capitalsList() {
+    let capArrayItemsOrange = []
+    let capArrayItemsBlack = []
+    
+    for (const property in listOfCapitals) {
+        console.log(`${property}: ${listOfCapitals[property]}`);
+        
+        let myValue = listOfCapitals[property]
+        
+        if (myValue === 'orange') {
+            capArrayItemsOrange.push(property)
+        }
+        else {
+            capArrayItemsBlack.push(property)
+        }
+    }
+    let capArrayItemsOrange2 = []
+
+    for (const property of capArrayItemsOrange) {
+        capArrayItemsOrange2.push(<li>{property}</li>)
+    }
+
+
+    let capArrayItemsBlack2 = []
+
+    for (const property of capArrayItemsBlack) {
+        capArrayItemsBlack2.push(<li>{property}</li>)
+    }
+
+    return (
+    <div> 
+                <ul>
+                  
+                    <font color = "orange">
+                    {capArrayItemsOrange2}
+                    </font>
+                    <font color= "black">
+                    {capArrayItemsBlack2}
+                    </font>
+                    
+                </ul>
+        </div>
+    );
+}
